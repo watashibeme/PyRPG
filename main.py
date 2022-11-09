e3f51ac1e2026ee533224b37e0a524768bca7040
Alireza Rezaei <83181899+watashibeme@users.noreply.github.com>
2022-11-09 19:12:43 +0330

    Update main.py
    تغییر روش ایجاد پسورد تصادفی
    و بهینه سازی کد

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-letters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-]
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import secrets
+import string
 
-print("Welcome to the PyPassword Generator!")
+alphabet=string.ascii_letters + string.digits + string.punctuation
+print("Welcome!")
+print("This program generates a password , based on user input ")
+# fix password length(read from user input)
+print("How many characters you want?")
+pwd_length=int(input())
+# generate a password string
+pwd=''
+for i in range(pwd_length):
+    pwd+=''.join(secrets.choice(alphabet))
 
-nr_letters = int(input("How many letters would you like in your password?\n"))
-
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+print("Here's your password")
+print(pwd)
