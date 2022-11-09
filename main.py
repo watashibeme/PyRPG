4a680aa0a27bd67b9353207c92931b542acb4a08
Alireza Rezaei <83181899+watashibeme@users.noreply.github.com>
2022-11-09 19:29:12 +0330

    Update main.py
    Reformat code to conform to pep8:E225
    https://www.flake8rules.com/rules/E225.html
    https://peps.python.org/pep-0008/#pet-peeves

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import secrets
 import string
 
-alphabet=string.ascii_letters + string.digits + string.punctuation
+alphabet = string.ascii_letters + string.digits + string.punctuation
 print("Welcome!")
 print("This program generates a password , based on user input ")
 # fix password length(read from user input)
 print("How many characters you want?")
-pwd_length=int(input())
+pwd_length = int(input())
 # generate a password string
-pwd=''
+pwd = ''
 for i in range(pwd_length):
-    pwd+=''.join(secrets.choice(alphabet))
+    pwd += ''.join(secrets.choice(alphabet))
 
 print("Here's your password")
 print(pwd)
