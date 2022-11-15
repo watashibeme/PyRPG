dc890e45fcd57bf9fef2c5e22c650271dcb1fd35
Alireza Rezaei <83181899+watashibeme@users.noreply.github.com>
2022-11-15 11:19:04 +0330

    introduce pyinputplus for validating input
    inputInt will only accept integer , it won't proceed otherwise

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import secrets
 import string
+import pyinputplus
 
 alphabet = string.ascii_letters + string.digits + string.punctuation
 print("Welcome!")
 print("This program generates a password , based on user input ")
 # fix password length(read from user input)
 print("How many characters you want?")
-# whatever read from input, is equal to password length
-pwd_length = int(input())
+print("(Please enter digits only)")
+# inputInt will only accept integer , it won't proceed otherwise
+pwd_length = (pyinputplus.inputInt())
+
+
 # generate a password string
 pwd = ''
-# for loop, range of loop equals to pwd_length, which is taken from user input 
 for i in range(pwd_length):
-    #join the empty pwd string with whatever secrets.choice generates from alphabet
-    #alphabet will contain letters,digits and punctuations(special characters like */-+ etc. )
+    # keep choosing from values in alphabet until it equals pwd_length
     pwd += ''.join(secrets.choice(alphabet))
 
 print("Here's your password")
